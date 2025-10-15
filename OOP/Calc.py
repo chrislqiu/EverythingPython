56d30ea695d8703adaec6ea85b6338cf4cb44544
Chris <89666114+chrislqiu@users.noreply.github.com>
2025-10-15 18:38:44 -0400

    calculator and assert test

--- OOP/Calc.py
+++ OOP/Calc.py
@@ -19,3 +19,11 @@ class Calc:
         if b == 0:
             return "Error: Division by zero"
         return a / b
+
+def test_operations():
+
+    calc = Calc()
+    assert calc.add(5, 3) == 8
+    assert calc.subtract(5, 3) == 2
+    assert calc.multiply(5, 3) == 15
+    assert calc.divide(6, 3) == 2
