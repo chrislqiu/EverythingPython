b83f405497f48b71400064c1a87e1da287aa86b3
unknown <chris.qiu52@gmail.com>
2025-10-18 17:34:55 -0400

    complete avg heartrate of male and female

--- API/RestAPI.py
+++ API/RestAPI.py
@@ -12,7 +12,20 @@ def avg_heartbeat(gender):
     while True:
         res = requests.get(f"{url}?page={page}&sex={gender}")
         data = res.json()
+        
+        #runner is now an object we need to access "avgheartbeat" of each runner
+        for runner in data["data"]:
+            heart_rate.append(runner["avgheartbeat"])
 
-    
+        if page >= data["total_pages"]:
+            break
 
-print(avg_heartbeat("female"))
+        page+=1
+
+        if heart_rate:
+            return sum(heart_rate) / len(heart_rate)
+        else:
+            return 0
+
+print(f"The average heart rate of female runners is: " + str(avg_heartbeat("female")))
+print(f"The average heart rate of male runners is: " + str(avg_heartbeat("male")))
