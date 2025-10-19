ec5157448c06e0a57480bbb1aa8c653bbdf7127e
unknown <chris.qiu52@gmail.com>
2025-10-18 21:41:11 -0400

    Key works from env

--- API/Nasa.py
+++ API/Nasa.py
@@ -1,10 +1,13 @@
 import os
 import requests
+from dotenv import load_dotenv
 
+load_dotenv()
 KEY = os.environ.get("NASA_API_KEY")
-url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={KEY}"
+url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2020-07-01&api_key={KEY}"
 
 def NasaImg():
+    print(url)
     res = requests.get(url)
     data = res.json()
 
