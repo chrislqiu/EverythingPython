5fdee00cc28b99605fff2dbc62ad3533b06f6bd9
unknown <chris.qiu52@gmail.com>
2025-10-21 19:34:42 -0400

    intro agent done

--- AI/HelloWorldAgent.py
+++ AI/HelloWorldAgent.py
@@ -9,18 +9,28 @@ class AgentState(TypedDict):
 # pass in the state and output the updated state 
 def greeting_node(state: AgentState) -> AgentState:
     """Simple node that adds greeting message to the state"""
-    state['msg'] = "Hey" + state['msg'] + ", how is your day going?"
+    state['msg'] = "Hey " + state['msg'] + ", how is your day going?"
 
     return state
 
+# setting the state for the graph
 graph = StateGraph(AgentState)
 
+# adding node
 graph.add_node("greeter", greeting_node)
 
+# creating entry and finish point
 graph.set_entry_point("greeter")
 graph.set_finish_point("greeter")
 
+# compile graph
 app = graph.compile()
 
+"""
 with open("greetingGraph.png", "wb") as f:
     f.write(app.get_graph().draw_mermaid_png())
+"""
+
+result = app.invoke({"msg": "Bob"})
+
+print(result["msg"])
